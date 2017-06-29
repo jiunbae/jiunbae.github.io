e5522e761a667993e6c4a31fe026f4d745976b03
MaybeS <maytryark@gmail.com>
2017-06-29 14:40:20 +0900

    Update about

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,31 +6,30 @@ function isRetina() {
 					  (min--moz-device-pixel-ratio: 1.5),\
 					  (-o-min-device-pixel-ratio: 3/2),\
 					  (min-resolution: 1.5dppx)";
- 
 	if (window.devicePixelRatio > 1)
 		return true;
- 
 	if (window.matchMedia && window.matchMedia(mediaQuery).matches)
 		return true;
- 
 	return false;
 };
  
- 
 function retina() {
-	
 	if (!isRetina())
 		return;
-	
 	$("img.2x").map(function(i, image) {
-		
 		var path = $(image).attr("src");
-		
 		path = path.replace(".png", "@2x.png");
 		path = path.replace(".jpg", "@2x.jpg");
-		
 		$(image).attr("src", path);
 	});
 };
+
+// progress bar
+jQuery('.skillbar').each(function(){
+    jQuery(this).find('.skillbar-bar').animate({
+        width:jQuery(this).attr('data-percent')
+    },2000);
+});
+
  
 $(document).ready(retina);
